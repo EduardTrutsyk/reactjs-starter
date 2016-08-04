eeab1d272887c38af3ef2649a022e7bc82814849
Eduard_Trutsyk <ediktrutsik@gmail.com>
2016-08-04 19:52:08 +0300

    move to client folder

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -2,8 +2,6 @@ import Counter from './Counter/Counter';
 import Greeter from './Greeter/Greeter';
 import React from 'react';
 
-import './app.scss';
-
 const App = () => (
   <div className="app">
     <h1>React App Test!</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,60 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'inline-source-map',
-  //devtool: 'eval-source-map',
-  //devtool: 'eval',
-  entry: __dirname + '/src',
+  // devtool: 'eval-source-map',
+  // devtool: 'eval',
+  entry: path.join(__dirname, '/src/client'),
   output: {
-    path: __dirname + '/build',
-    filename: 'main.js'
+    path: path.join(__dirname, '/build/client'),
+    filename: 'main.js',
   },
 
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
 
   module: {
     loaders: [{
-          test: /\.json$/,
-          loader: "json"
-        }, {
-          test: /\.jsx?$/,
-          exclude: /(node_modules|bower_components)/,
-          loader: 'babel'
-        }, {
-          test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
-          loader: 'file-loader'
-        },
-        {
-          test: /\.scss$/,
-          include: /src/,
-          loader: ExtractTextPlugin.extract('css?sourceMap!postcss!sass?sourceMap')
-          //loaders: [ 'style', 'css?sourceMap', 'sass?sourceMap' ]
-          //loaders: ['style-loader', 'css-loader', 'postcss-loader']
-          //loaders: ExtractTextPlugin.extract('css!sass') //['style', 'css', 'sass']
-          //loader: ExtractTextPlugin.extract('style-loader', ['css-loader', 'postcss-loader', 'sass-loader'])
-          //loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader?sourceMap')
-          //loader: 'style!css!postcss!sass?sourceMap'
-        }
-      ]
+      test: /\.json$/,
+      loader: 'json',
+    }, {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+    }, {
+      test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+      loader: 'file-loader',
+    }, {
+      test: /\.scss$/,
+      include: /src/,
+      loader: ExtractTextPlugin.extract('css?sourceMap!postcss!sass?sourceMap'),
+      //loaders: [ 'style', 'css?sourceMap', 'sass?sourceMap' ]
+      //loaders: ['style-loader', 'css-loader', 'postcss-loader']
+      //loaders: ExtractTextPlugin.extract('css!sass') //['style', 'css', 'sass']
+      //loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader?sourceMap')
+      //loader: 'style!css!postcss!sass?sourceMap'
+    }],
   },
 
   postcss: [
-    require('autoprefixer')({
-      browsers: ['last 3 versions']
-    })
+    autoprefixer({
+      browsers: ['last 3 versions'],
+    }),
   ],
 
   plugins: [
-    //new CleanWebpackPlugin(['build'], { verbose: true }),
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: __dirname + '/src/index.tmpl.html'
+      template: path.join(__dirname, '/src/client/index.tmpl.html'),
     }),
     new ExtractTextPlugin('main.css', {
-      allChunks: true
-    })
+      allChunks: true,
+    }),
   ],
 
   devServer: {
@@ -65,6 +62,6 @@ module.exports = {
     colors: true,
     historyApiFallback: true,
     inline: true,
-    hot: true
-  }
+    hot: true,
+  },
 };
